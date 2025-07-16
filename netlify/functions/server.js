0d2bade7a65cf1366a3f181d6e32e7bcc72fc965
mghebro <giorgimgebrishvili2008@gmail.com>
2025-07-16 15:33:00 +0400

    server.js modified

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,4 +1,4 @@
-const AppleAuth = require('./src/app/Auth/apple-auth'); 
+const AppleAuth = require('../../src/app/Auth/apple-auth/apple-auth'); 
 const jwt = require('jsonwebtoken');
 const axios = require('axios');
 
